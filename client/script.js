120ffa244a3fd84ce5798d856ef1c281ff1f91c2
Preshfile <preshcharlies2014@gmail.com>
2023-03-23 21:10:28 +0100

    eppmit

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-//lets start by importing our icons from our assets folder
+//import icons from assets folder
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
@@ -7,59 +7,48 @@ const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
 
-//This loader is simply going to return 3 dots (...) to keep running until it fetches an answer to a question
+// is  to return 3 dots (...) to keep running until it fetches an answer to a question
 function loader(element) {
   element.textContent = '';
 
-  // set the load interval, a function that accepts another call back function
+  // set the load interval, 
   loadInterval = setInterval(() => {
-    //we want to do something - adding each of the three dots ever 300 milliseconds
     element.textContent += '.';
-    //if the elements reaches 3 dots, we want to reset it
+    //reset after 3 dots
     if (element.textContent === '....') {
-      //then we reset it if the dot reaches 3. This is going to repeat every 300 milliseconds
       element.textContent = '';
     }
   }, 300) 
- 
 }
 
-//create a function that would make the API type one letter by letter giving the feel of a robot or machine.
-//we don't want our entire text to simply appear instantly, we want it running
-//letter by letter so it could be perceived as a robot
+//letter by letter typing function to have a robot feel
 function typeText(element, text) {
-  //at the start, the index is going to be set to 0
   let index = 0;
 
-  //we create another interval with just 20 milliseconds
   let interval = setInterval(() => {
-    //lets check if index is lower than text dot length. If yes, it means we're still typing.
+    //check if index is lower than text dot length. If yes, it means still typing.
     if(index < text.length){
       element.innerHTML += text.chartAt(index); //this will get the character under a specific index in the text that AI is going to return.
       index++; //increment
     } else {
-      //we want to simply clear the interval
+      // simply clear the interval
       clearInterval(interval);
     }
   }, 20)
 }
 
-//we want to generate a unique ID for every single message, to be able to map over them
+// generate a unique ID for every single message, to be able to map over them
 function generateUniqueId(){
-  const timestamp = Date.now();//in javascript and many other languages, how you generate a unique id is using the current time and date - that is always unique.
-  const randomNumber = Math.random();//to make it even more random, we use these JavaSript built-in functions
-  const hexadecimalString = randomNumber.toString(16);//we can make it even more random by creating a hexadecimal string - 16 characters.
-  
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
   //finally we can return an id.
   return `id-${timestamp}-${hexadecimalString}`;//This will surely return a unique id.
 }
 
-//let's implement the chat stripe - the gray and the dark gray area, including who is speaking: AI or US.
-//we will get the value of the message and pass it a unique id
+// implement the chat stripe - the gray and the dark gray area, including who is speaking: AI or US.
 function chatStripe (isAi, value, uniqueId ){
   return(
-    //this function will return a template string not regular strings
-    //with regular strings, you cannot create spaces or enters, but with template strings, you can
     `
       <div class="wrapper ${isAi && 'ai'}">
         <div class="chat">
@@ -76,7 +65,7 @@ function chatStripe (isAi, value, uniqueId ){
   )
 }
 
-//let's create our handle submit function which is going to be the trigger to get the Ai generated response. it's going to be an async function to take an event as the first and only parameter
+// handle submit function which is going to be the trigger to get the Ai generated response. 
 const handleSubmit = async (e) => {
   //the default browser behaviour for when you submit a form is to reload the browser. But we dont want that to happen so we first prevent it:
   e.preventDefault();//This will prevent the default behaviour of the browser.
@@ -105,7 +94,6 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
 //fetch data from server
-//this is the place we can fetch the data from server, which means that we can get the bot's response
 const response = await fetch(`http://localhost:17258`, {
   method: 'POST',
   headers: {
@@ -117,15 +105,12 @@ const response = await fetch(`http://localhost:17258`, {
 })
 
 clearInterval(loadInterval);
-messageDiv.innerHTML = ''; //because were not sure at which point in the loading are we right now at the point when we fetch - might be one dot 2 or three dots
-//but we want to clear it to be empty at the time we want to add our message.
+messageDiv.innerHTML = ''; 
   if(response.ok){
-    const data = await response.json();//this is giving us the actual response coming from the backend but 
+    const data = await response.json();
     //we need to parse it
     const parsedData = data.bot.trim();
-    // console.log({parsedData})
-
-    //and finally we can parse it through our type text function which we created before
+ 
     typeText(messageDiv, parsedData);
   } else{
     // if we have an error
@@ -138,13 +123,9 @@ messageDiv.innerHTML = ''; //because were not sure at which point in the loading
 }
 
 //to be able to see the changes made to our handle submit, we have to call it
-form.addEventListener('submit', handleSubmit);//its going to be a listener for a submit event. Once we submit, we want to call the handleSubmit function)
-//we developers are used to submitting with enter key.
+form.addEventListener('submit', handleSubmit);
 form.addEventListener('keyup', (e) => {
   if (e.keyCode === 13) { //13 is the enter key
     handleSubmit(e);
   }
 }) 
-
-//create a backend application that is going to make a 
-//a call to the ope AI's chat GPD API. Lets get started with that in the server folder
--- server/server.js
+++ server/server.js
@@ -1,74 +1,51 @@
-//here we do all the setups and all the configurations to be all to call openai's API
-//but first we need the API key and we can get it straight from https://platform.openai.com/
-//to get started, click your profile on top right and click view API keys
-//right there you will see 'create a new secrete key'
-//finally copy it
-//Then back inside our code, open the file explorer, and then in the root of our our application,
-//create a new .env file(make sure it's in the root and not in the server folder).
-//write this OPENAI_OPEN_KEY"" inside the .env file and paste the generated key inside the string, thats all.
 
-//now lets start writing inside our server.js file
-
-//first let's import a couple of things
-//the .env should be inside theserver folder because thats where we should use it
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
 
-//to be able to use the dotenv vaiable
 dotenv.config();
-
 
 //configuration
 const configuration = new Configuration({
-    apikey: process.env.OPENAI_API_KEY, //openai knows this key, and with it it should give us responses
+    apikey: process.env.OPENAI_API_KEY, //openai knows this key, and with it it should give responses
 });
 
-//then we need to create an instance of openai
+// create an instance of openai
 const openai = new OpenAIApi(configuration);
 
-//we need to initialize our express application
+// initialize our express application
 const app = express();
-//set up a couple of middlewears
-app.use(cors());//This is going to allow us to make those cross origin requests and allow our server to be called from the front end
-app.use(express.json());//this is going to allow us to pass jason from the front end to the back end
 
-//create a dummy root route. 
-//its going to be an asynchronous function that will accept a request and a response
+//set up a couple of middlewears
+app.use(cors());
+app.use(express.json());
+
+//create a dummy root route.
 app.get('/', async (req, res) => {
     res.status(200).send({
         message: 'Hello from Eppmit',
     })
 });
 
-//the post allow allows us to have a body or a payload so
+//the post allow allows for a body or a payload
 app.post('/', async (req, res) => {
-    //lets wrapp everything in a try and catch block
+    //wrap everything in a try and catch block
     try {
         const prompt = req.body.prompt;
         //get a response from th open API. Its a function that accepts an object
         const response = await openai.createCompletion({
-            //there are a lot of different things we can pass into this function
-            //go to this https://platform.openai.com/examples and type code, click on natural language Api and click on openplayground
-            //we will see on the right side different language models available from which we can choose from
-            //code models are capable of generating code. 
-            //in our available options, text DaVinci 3 is found to be more capable because...
-            //it can understand text as well as code and it can produce higher quality output.
-            //so we gonna switch to text DaVinci 3. now click view code
-            //This is going to give you a snippet of code, we only copy the parameters and paste them in this function
-
-            model:"text-davinci-003",
-            prompt:`${prompt}`,
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
             temperature: 0, //higher temperature value means the model will take more risks. in this case we don't want a lot of risk so we keep it down
-            max_tokens:3000,//The maximum number of tokens to generate in a completion. Giving it 3000 means it can give pretty long responses
+            max_tokens: 3000,//The maximum number of tokens to generate in a completion. Giving it 3000 means it can give pretty long responses
             top_p: 1,
             frequency_penalty: 0.5,//means that it's not going to repeat similar sentences often, so we can set that to 0.5, hence its less likely to repeat itself 
             presence_penalty: 0,
-           //we don't need a stop
+            //we don't need a stop
         });
-        
-        //once we get the responses, we need to send it back to the frontend
+
+        //send response back to the frontend
         res.status(200).send({
             bot: response.data.choices[0].text
         })
@@ -80,7 +57,5 @@ app.post('/', async (req, res) => {
 
 })
 
-//the above is all you need to do to be able to get a response from the most powerful AI
-
-//now we have to make sure that our server always listens for new requests
+// make sure that our server always listens for new requests
 app.listen(17258, () => console.log(`Server is running on port http://localhost:17258`));

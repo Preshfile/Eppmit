52670613149241d97cdfcee7b2735a50c898ece8
Preshfile <preshcharlies2014@gmail.com>
2023-03-30 19:46:32 +0100

    update added

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-//import icons from assets folder
+//lets start by importing our icons from our assets folder
 import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
@@ -7,48 +7,59 @@ const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
 
-// is  to return 3 dots (...) to keep running until it fetches an answer to a question
+//This loader is simply going to return 3 dots (...) to keep running until it fetches an answer to a question
 function loader(element) {
   element.textContent = '';
 
-  // set the load interval, 
+  // set the load interval, a function that accepts another call back function
   loadInterval = setInterval(() => {
+    //we want to do something - adding each of the three dots ever 300 milliseconds
     element.textContent += '.';
-    //reset after 3 dots
+    //if the elements reaches 3 dots, we want to reset it
     if (element.textContent === '....') {
+      //then we reset it if the dot reaches 3. This is going to repeat every 300 milliseconds
       element.textContent = '';
     }
   }, 300) 
+ 
 }
 
-//letter by letter typing function to have a robot feel
+//create a function that would make the API type one letter by letter giving the feel of a robot or machine.
+//we don't want our entire text to simply appear instantly, we want it running
+//letter by letter so it could be perceived as a robot
 function typeText(element, text) {
+  //at the start, the index is going to be set to 0
   let index = 0;
 
+  //we create another interval with just 20 milliseconds
   let interval = setInterval(() => {
-    //check if index is lower than text dot length. If yes, it means still typing.
+    //lets check if index is lower than text dot length. If yes, it means we're still typing.
     if(index < text.length){
       element.innerHTML += text.chartAt(index); //this will get the character under a specific index in the text that AI is going to return.
       index++; //increment
     } else {
-      // simply clear the interval
+      //we want to simply clear the interval
       clearInterval(interval);
     }
   }, 20)
 }
 
-// generate a unique ID for every single message, to be able to map over them
+//we want to generate a unique ID for every single message, to be able to map over them
 function generateUniqueId(){
-  const timestamp = Date.now();
-  const randomNumber = Math.random();
-  const hexadecimalString = randomNumber.toString(16);
+  const timestamp = Date.now();//in javascript and many other languages, how you generate a unique id is using the current time and date - that is always unique.
+  const randomNumber = Math.random();//to make it even more random, we use these JavaSript built-in functions
+  const hexadecimalString = randomNumber.toString(16);//we can make it even more random by creating a hexadecimal string - 16 characters.
+  
   //finally we can return an id.
   return `id-${timestamp}-${hexadecimalString}`;//This will surely return a unique id.
 }
 
-// implement the chat stripe - the gray and the dark gray area, including who is speaking: AI or US.
+//let's implement the chat stripe - the gray and the dark gray area, including who is speaking: AI or US.
+//we will get the value of the message and pass it a unique id
 function chatStripe (isAi, value, uniqueId ){
   return(
+    //this function will return a template string not regular strings
+    //with regular strings, you cannot create spaces or enters, but with template strings, you can
     `
       <div class="wrapper ${isAi && 'ai'}">
         <div class="chat">
@@ -65,7 +76,7 @@ function chatStripe (isAi, value, uniqueId ){
   )
 }
 
-// handle submit function which is going to be the trigger to get the Ai generated response. 
+//let's create our handle submit function which is going to be the trigger to get the Ai generated response. it's going to be an async function to take an event as the first and only parameter
 const handleSubmit = async (e) => {
   //the default browser behaviour for when you submit a form is to reload the browser. But we dont want that to happen so we first prevent it:
   e.preventDefault();//This will prevent the default behaviour of the browser.
@@ -94,6 +105,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
 //fetch data from server
+//this is the place we can fetch the data from server, which means that we can get the bot's response
 const response = await fetch(`http://localhost:17258`, {
   method: 'POST',
   headers: {
@@ -105,12 +117,15 @@ const response = await fetch(`http://localhost:17258`, {
 })
 
 clearInterval(loadInterval);
-messageDiv.innerHTML = ''; 
+messageDiv.innerHTML = ''; //because were not sure at which point in the loading are we right now at the point when we fetch - might be one dot 2 or three dots
+//but we want to clear it to be empty at the time we want to add our message.
   if(response.ok){
-    const data = await response.json();
+    const data = await response.json();//this is giving us the actual response coming from the backend but 
     //we need to parse it
     const parsedData = data.bot.trim();
- 
+    // console.log({parsedData})
+
+    //and finally we can parse it through our type text function which we created before
     typeText(messageDiv, parsedData);
   } else{
     // if we have an error
@@ -123,9 +138,13 @@ messageDiv.innerHTML = '';
 }
 
 //to be able to see the changes made to our handle submit, we have to call it
-form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', handleSubmit);//its going to be a listener for a submit event. Once we submit, we want to call the handleSubmit function)
+//we developers are used to submitting with enter key.
 form.addEventListener('keyup', (e) => {
   if (e.keyCode === 13) { //13 is the enter key
     handleSubmit(e);
   }
 }) 
+
+//create a backend application that is going to make a 
+//a call to the ope AI's chat GPD API. Lets get started with that in the server folder
